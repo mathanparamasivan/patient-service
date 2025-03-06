454549f9a2cc715c383ef72eb7310d628095910a
mathanpandi.p <mathanpandi.paramasivan@ideas2it.com>
2025-03-06 17:04:34 +0530

    Docker integration

---
+++ start_test_container.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Stop Docker Compose containers
+docker-compose -f docker-compose.test.yml down
+
+REM Get latest test docker image
+docker build -t patient-service:latest .
+
+REM start Docker Compose containers
+docker-compose -f docker-compose.test.yml up -d
+
+REM Show running containers
+docker ps
